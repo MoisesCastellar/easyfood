fc43d617c1b244b02fd0007337220774bb73107c
Moises Castellar Solano <mctecnia@gmail.com>
2018-11-19 12:20:49 -0500

    cuarto commit

--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,15 @@
 let imagenes = document.querySelectorAll('.gallery__img');
-let modal = document.getElementById('modal');
-let img = document.getElementById('modal__img');
-let boton = document.getElementById('modal__boton');
+let modal = document.querySelector('#modal');
+let img = document.querySelector('#modal__img');
+let boton = document.querySelector('#modal__boton');
 
-for (let i = 0; i < imaganes.length; i++) {
-    imagenes[i].addEventListener("click", function(e) {
-        modal.classList.toggle("modal--open");
+for (let i = 0; i < imagenes.length; i++) {
+    imagenes[i].addEventListener('click', function(e) {
         let src = e.target.src;
         img.setAttribute("src", src);
+        modal.classList.toggle("modal--open");
     });   
 }
 boton.addEventListener("click", function() {
     modal.classList.toggle("modal--open");
-});  
+});
